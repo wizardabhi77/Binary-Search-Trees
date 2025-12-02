b43a3d0ecb8f6df06cfbb8ee90aab27e22bd5434
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-12-02 19:16:25 +0530

    Add Tree class and Node class

---
+++ binaryTree.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor (data) {
+        this.data = data;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+
+
+}
+
+class Tree {
+    constructor (initArr) {
+        this.initArr = initArr;
+        this.rootNode = null;
+    }
+}
