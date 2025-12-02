c3d8398b3341ca0f8e51f5e3d3584c60a6c2dbde
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-12-02 20:50:31 +0530

    Add buildTree method

--- binaryTree.js
+++ binaryTree.js
@@ -9,8 +9,48 @@ class Node {
 }
 
 class Tree {
+    
     constructor (initArr) {
-        this.initArr = initArr;
-        this.rootNode = null;
+        
+        initArr.sort((a, b) => a - b);
+        let noDupArr = removeDuplicates(initArr);
+        this.rootNode = this.buildTree(noDupArr, 0, noDupArr.length-1);
     }
+
+    buildTree (arr,start,end) {
+        
+        
+        if(start>end){
+            return null;
+        }
+        
+        let mid = Math.floor((start+end)/2);
+
+        let root = new Node(arr[mid]);
+        
+        
+        root.leftNode = this.buildTree(arr, start, mid-1);
+        root.rightNode = this.buildTree(arr, mid+1, end);
+        
+        
+        return root;
+    }
+
 }
+
+function removeDuplicates(array){
+    
+    
+    let noDup = [array[0]];
+    
+    for (let i=1; i<array.length; i++){
+        if(array[i] !== array[i-1]){
+            noDup.push(array[i]);
+        }
+       
+    }
+    
+    return noDup;
+}
+
+export default Tree;
---
+++ main.js
@@ -0,0 +1,18 @@
+import Tree from './binaryTree.js';
+
+let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.rightNode !== null) {
+    prettyPrint(node.rightNode, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.leftNode !== null) {
+    prettyPrint(node.leftNode, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
+prettyPrint(tree.rootNode);
